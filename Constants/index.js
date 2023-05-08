4406a097418bea2796d5dec6f4b0d95c237a75a0
mahrukh <mahrukh.khalid@reetro.io>
2023-05-08 12:10:48 +0500

    no message

---
+++ Constants/index.js
@@ -0,0 +1,4 @@
+export const GuestUrl = "https://www.reetro.app/board/6343aae05465136fe0c01c5b/639828fe9af521a4328bd447"
+export const BaseUrl = `https://reetro-io-staging.herokuapp.com/`
+export const ProductionURL = 'https://www.reetro.app/'
+export const GuestURLProduc ='https://www.reetro.app/board/6407200daba228540fb420c2/6423cf0bd8df55a1128daa75'
---
+++ cypress/e2e/features/password_reset.feature
@@ -0,0 +1,8 @@
+Feature: Password Reset
+
+Scenario: Reset Password Successfully
+  Given I am on the Reetro website
+  When I click on the Forgot Password link
+  Then I should be on the Forgot Password page
+  
+  
